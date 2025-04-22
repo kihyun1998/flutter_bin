86b4626b8f0fd3388b7d4bbf02a697d69e1c2520
박기현 <kihyun1998@naver.com>
2025-04-22 16:26:19 +0900

    Update plugin_integration_test.dart

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -6,14 +6,8 @@
 // For more information about Flutter integration tests, please see
 // https://flutter.dev/to/integration-testing
 
-import 'package:flutter_bin/flutter_bin.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final FlutterBin plugin = FlutterBin();
-  });
 }
